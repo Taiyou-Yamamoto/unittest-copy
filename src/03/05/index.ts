73429cfda841c1bf555f82e86e8f2f02a003da61
山本　太陽 <yamamototaiyou.811@gmail.com>
2025-04-07 00:14:13 +0900

    Typescriptでは検知できないエラーについて学習

--- src/03/05/index.ts
+++ src/03/05/index.ts
@@ -2,23 +2,25 @@ export class RangeError extends Error {}
 
 function checkRange(value: number) {
   if (value < 0 || value > 100) {
-    throw new RangeError("入力値は0〜100の間で入力してください");
+    throw new RangeError('入力値は0〜100の間で入力してください');
   }
 }
 
 export function add(a: number, b: number) {
-  checkRange(a);
-  checkRange(b);
+  // checkRange(a);
+  // checkRange(b);
   const sum = a + b;
   if (sum > 100) {
     return 100;
   }
   return sum;
 }
+// add(1, 2);
+// add('1', '2');
 
 export function sub(a: number, b: number) {
-  checkRange(a);
-  checkRange(b);
+  // checkRange(a);
+  // checkRange(b);
   const sum = a - b;
   if (sum < 0) {
     return 0;
