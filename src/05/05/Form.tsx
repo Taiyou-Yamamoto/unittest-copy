0ece38cd063d8838321f5998825da98ec9a2cb5e
山本　太陽 <yamamototaiyou.811@gmail.com>
2025-05-26 01:36:28 +0900

    テスト

--- src/05/05/Form.tsx
+++ src/05/05/Form.tsx
@@ -1,12 +1,15 @@
-import { useId, useState } from "react";
-import { Agreement } from "./Agreement";
-import { InputAccount } from "./InputAccount";
+import { useId, useState } from 'react';
+import { Agreement } from './Agreement';
+import { InputAccount } from './InputAccount';
 
 export const Form = () => {
   const [checked, setChecked] = useState(false);
   const headingId = useId();
   return (
     <form aria-labelledby={headingId}>
+      {/* aria-labelledby は親子関係を問わず、
+      「この要素の名前は“あの要素”に書いてあるよ」
+      と“参照で名前付け”するための属性だよ */}
       <h2 id={headingId}>新規アカウント登録</h2>
       <InputAccount />
       <Agreement
