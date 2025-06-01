4e228e9563109cf2f1e1a65856fe45aafce59b9b
山本　太陽 <yamamototaiyou.811@gmail.com>
2025-06-01 22:27:33 +0900

    画像がアップロードできないのでコメントアウト

--- src/components/templates/MyPostsCreate/PostForm/index.test.tsx
+++ src/components/templates/MyPostsCreate/PostForm/index.test.tsx
@@ -1,9 +1,9 @@
-import { handleGetMyProfile } from "@/services/client/MyProfile/__mock__/msw";
-import { mockUploadImage } from "@/services/client/UploadImage/__mock__/jest";
-import { selectImageFile, setupMockServer } from "@/tests/jest";
-import { render, screen, waitFor } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import { PostForm } from ".";
+import { handleGetMyProfile } from '@/services/client/MyProfile/__mock__/msw';
+import { mockUploadImage } from '@/services/client/UploadImage/__mock__/jest';
+import { selectImageFile, setupMockServer } from '@/tests/jest';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { PostForm } from '.';
 
 const user = userEvent.setup();
 
@@ -11,24 +11,17 @@ function setup() {
   const onClickSave = jest.fn();
   const onValid = jest.fn();
   const onInvalid = jest.fn();
-  render(
-    <PostForm
-      title="新規記事"
-      onClickSave={onClickSave}
-      onValid={onValid}
-      onInvalid={onInvalid}
-    />
-  );
+  render(<PostForm title='新規記事' onClickSave={onClickSave} onValid={onValid} onInvalid={onInvalid} />);
   async function typeTitle(title: string) {
-    const textbox = screen.getByRole("textbox", { name: "記事タイトル" });
+    const textbox = screen.getByRole('textbox', { name: '記事タイトル' });
     await user.type(textbox, title);
   }
   async function saveAsPublished() {
-    await user.click(screen.getByRole("switch", { name: "公開ステータス" }));
-    await user.click(screen.getByRole("button", { name: "記事を公開する" }));
+    await user.click(screen.getByRole('switch', { name: '公開ステータス' }));
+    await user.click(screen.getByRole('button', { name: '記事を公開する' }));
   }
   async function saveAsDraft() {
-    await user.click(screen.getByRole("button", { name: "下書き保存する" }));
+    await user.click(screen.getByRole('button', { name: '下書き保存する' }));
   }
   return {
     typeTitle,
@@ -42,29 +35,29 @@ function setup() {
 
 setupMockServer(handleGetMyProfile());
 
-test("不適正内容で「下書き保存」を試みると、バリデーションエラーが表示される", async () => {
-  const { saveAsDraft } = setup();
-  await saveAsDraft();
-  await waitFor(() =>
-    expect(
-      screen.getByRole("textbox", { name: "記事タイトル" })
-    ).toHaveErrorMessage("1文字以上入力してください")
-  );
-});
-
-test("不適正内容で「下書き保存」を試みると、onInvalid イベントハンドラーが実行される", async () => {
+test('不適正内容で「下書き保存」を試みると、バリデーションエラーが表示される', async () => {
   const { saveAsDraft, onClickSave, onValid, onInvalid } = setup();
   await saveAsDraft();
+  await waitFor(() => expect(screen.getByRole('textbox', { name: '記事タイトル' })).toHaveErrorMessage('1文字以上入力してください'));
+  //冗長なので統一
   expect(onClickSave).toHaveBeenCalled();
   expect(onValid).not.toHaveBeenCalled();
   expect(onInvalid).toHaveBeenCalled();
 });
 
-test("適正内容で「下書き保存」を試みると、onValid イベントハンドラーが実行される", async () => {
+// test('不適正内容で「下書き保存」を試みると、onInvalid イベントハンドラーが実行される', async () => {
+//   const { saveAsDraft, onClickSave, onValid, onInvalid } = setup();
+//   await saveAsDraft();
+//   expect(onClickSave).toHaveBeenCalled();
+//   expect(onValid).not.toHaveBeenCalled();
+//   expect(onInvalid).toHaveBeenCalled();
+// });
+
+test('適正内容で「下書き保存」を試みると、onValid イベントハンドラーが実行される', async () => {
   mockUploadImage();
   const { typeTitle, saveAsDraft, onClickSave, onValid, onInvalid } = setup();
   const { selectImage } = selectImageFile();
-  await typeTitle("私の技術記事");
+  await typeTitle('私の技術記事');
   await selectImage();
   await saveAsDraft();
   expect(onClickSave).toHaveBeenCalled();
@@ -72,10 +65,9 @@ test("適正内容で「下書き保存」を試みると、onValid イベント
   expect(onInvalid).not.toHaveBeenCalled();
 });
 
-test("適正内容で「記事を公開」を試みると、onClickSave イベントハンドラーのみ実行される", async () => {
-  const { typeTitle, saveAsPublished, onClickSave, onValid, onInvalid } =
-    setup();
-  await typeTitle("私の技術記事");
+test('適正内容で「記事を公開」を試みると、onClickSave イベントハンドラーのみ実行される', async () => {
+  const { typeTitle, saveAsPublished, onClickSave, onValid, onInvalid } = setup();
+  await typeTitle('私の技術記事');
   await saveAsPublished();
   expect(onClickSave).toHaveBeenCalled();
   expect(onValid).not.toHaveBeenCalled();
--- src/components/templates/MyPostsCreate/PostForm/index.tsx
+++ src/components/templates/MyPostsCreate/PostForm/index.tsx
@@ -1,17 +1,11 @@
-import { PostFormFooter } from "@/components/molecules/PostFormFooter";
-import { PostFormHeroImage } from "@/components/molecules/PostFormHeroImage";
-import { PostFormInfo } from "@/components/molecules/PostFormInfo";
-import { TextareaWithInfo } from "@/components/molecules/TextareaWithInfo";
-import { createMyPostInputSchema } from "@/lib/schema/MyPosts";
-import { PostInput } from "@/pages/api/my/posts";
-import { zodResolver } from "@hookform/resolvers/zod";
-import {
-  FieldValues,
-  SubmitErrorHandler,
-  SubmitHandler,
-  useForm,
-} from "react-hook-form";
-import styles from "./styles.module.css";
+import { PostFormFooter } from '@/components/molecules/PostFormFooter';
+import { PostFormInfo } from '@/components/molecules/PostFormInfo';
+import { TextareaWithInfo } from '@/components/molecules/TextareaWithInfo';
+import { createMyPostInputSchema } from '@/lib/schema/MyPosts';
+import { PostInput } from '@/pages/api/my/posts';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { FieldValues, SubmitErrorHandler, SubmitHandler, useForm } from 'react-hook-form';
+import styles from './styles.module.css';
 
 type Props<T extends FieldValues = PostInput> = {
   title: string;
@@ -32,34 +26,15 @@ export const PostForm = (props: Props) => {
     resolver: zodResolver(createMyPostInputSchema),
   });
   return (
-    <form
-      aria-label={props.title}
-      className={styles.module}
-      onSubmit={handleSubmit(props.onValid, props.onInvalid)}
-    >
+    <form aria-label={props.title} className={styles.module} onSubmit={handleSubmit(props.onValid, props.onInvalid)}>
       <div className={styles.content}>
         <div className={styles.meta}>
           <PostFormInfo register={register} control={control} errors={errors} />
-          <PostFormHeroImage
-            register={register}
-            setValue={setValue}
-            name="imageUrl"
-            error={errors.imageUrl?.message}
-          />
+          {/* <PostFormHeroImage register={register} setValue={setValue} name='imageUrl' error={errors.imageUrl?.message} /> */}
         </div>
-        <TextareaWithInfo
-          {...register("body")}
-          title="本文"
-          rows={20}
-          error={errors.body?.message}
-        />
+        <TextareaWithInfo {...register('body')} title='本文' rows={5} error={errors.body?.message} />
       </div>
-      <PostFormFooter
-        isSubmitting={isSubmitting}
-        register={register}
-        control={control}
-        onClickSave={props.onClickSave}
-      />
+      <PostFormFooter isSubmitting={isSubmitting} register={register} control={control} onClickSave={props.onClickSave} />
       {props.children}
     </form>
   );
